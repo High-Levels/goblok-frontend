33cef8e09061f9159d084b55f0778b0b6f2b058c
IdinGanteng <kanggayus101@gmail.com>
2023-05-22 11:24:14 +0700

    setup log&reg

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button } from "primereact/button";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigasi=useNavigate();
   return (
     <>
       <div>Dashboard</div>
       <div className="card flex justify-content-center">
-        <Button label="Check" icon="pi pi-check" />
+        <Button label="Check" icon="pi pi-check" onClick={()=>navigasi("/register")} />
       </div>
     </>
   );
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { Password } from 'primereact/password';
+import { InputText } from 'primereact/inputtext';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
 
 const Register = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(userName, password)
+  }
   return (
-    <div>Register</div>
+    <>
+    <div className="flex justify-content-center">
+        <Card title="Register" className="mt-8">
+          <form>
+            <div className="flex flex-column mb-3">
+              <label>Username</label>
+              <InputText
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-column mb-3">
+              <label>Password</label>
+              <Password
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                toggleMask
+              />
+            </div>
+            <Button label="Submit" className="w-full" onClick={handleSubmit} type="submit" icon="pi pi-check"/>
+          </form>
+        </Card>
+      </div>
+    </>
   )
 }
 
