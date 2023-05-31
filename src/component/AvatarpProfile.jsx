9816bb25f61ae5dbb8069a6af1b971117f528020
IdinGanteng <kanggayus101@gmail.com>
2023-05-31 18:34:37 +0700

    dashboard update

---
+++ src/component/AvatarpProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AvatarpProfile = () => {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Cumque quia vitae excepturi libero architecto aliquam,
+              recusandae minima ratione. Autem, cumque.</div>
+
+    )
+}
+
+export default AvatarpProfile
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -7,12 +7,21 @@ import CardProfile from "../component/CardProfile";
 import Banner from "../component/Banner";
 
 const Dashboard = () => {
+  const [isLoggedIn] = useState(
+    localStorage.getItem('refreshToken')
+  )
   const navigasi = useNavigate();
   return (
     <>
       <div className="grid mt-8">
         <div className="col-14">
-          <Banner />
+          {
+            isLoggedIn ? (
+              <></>
+              ):(
+              <Banner/>
+            )
+          }
         </div>
         <div className="col-8 artikel">
           <div className="col-12">
